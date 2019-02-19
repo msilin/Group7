c972cf5cf21c1df682cd383bcb16629bdfaead2b
Spencer <spencer.gilson@gmail.com>
2019-02-18 17:46:54 -0800

    more styling

--- client/src/components/Home/AboutUs.vue
+++ client/src/components/Home/AboutUs.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="about-us" class="column">
     <h1 class="title has-text-centered">About Us</h1>
-    <article class="message is-info">
-        <div class="messsage-body" style="padding: 25px;">
-            <figure class="media-left">
-                <p class="image is-183x275">
-                    <img src="@/assets/aboutUs.jpeg" style="border-radius: 4px;">
-                </p>
-            </figure>
-            <br>
-            Located on Foothill Boulevard, we are the 
-            closest bike shop to Cal Poly, a quick ride 
-            from Downtown San Luis Obispo, and an easy 
-            commute from Cuesta College.
-        </div>
-    </article>
-    <article class="home-entry message is-info">
-        <div class="message-body">
-            No bike rack? No Problem! If you can't
-            get
-            your bike to us,
-            we'll come to you! We offer pickup and delivery for repairs
-            and new bike purchases! Contact us for details.
-        </div>
-    </article>
+    <span class="box">
+        <article class="message is-info">
+            <div class="messsage-body" style="padding: 25px;">
+                <figure class="media-left">
+                    <p class="image is-183x275">
+                        <img src="@/assets/aboutUs.jpeg" style="border-radius: 4px;">
+                    </p>
+                </figure>
+                <br>
+                Located on Foothill Boulevard, we are the 
+                closest bike shop to Cal Poly, a quick ride 
+                from Downtown San Luis Obispo, and an easy 
+                commute from Cuesta College.
+            </div>
+        </article>
+        <article class="home-entry message is-info">
+            <div class="message-body">
+                No bike rack? No Problem! If you can't
+                get
+                your bike to us,
+                we'll come to you! We offer pickup and delivery for repairs
+                and new bike purchases! Contact us for details.
+            </div>
+        </article>
+    </span>
   </div>
 </template>
 
